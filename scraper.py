aa4bf13ea810b0fecab40c661c7f2dba7785002d
Alexander Richter <alexrichterxyz@gmail.com>
2026-04-17 19:03:50 +1000

    clean rewrite

--- scraper.py
+++ scraper.py
@@ -1,54 +1,31 @@
 import asyncio
 import aiohttp
-import json
+from abc import ABC, abstractmethod
 
+class FastScraper(ABC):
+	timeout: int = 10
 
-class Response:
-    
-    def __init__(self, status=None, text=None, headers=None, cookies=None, url=None):
-        self.status = status
-        self.text = text
-        self.headers = headers
-        self.cookies = cookies
-        self.url = url
+	@property
+	def client_timeout(self):
+		return aiohttp.ClientTimeout(total=self.timeout)
 
-    def ok(self):
-        return self.status < 400
-    
-    def json(self):
-        return json.loads(self.text)
-        
-        
-class Scraper:
-        
-    async def request(self, url, method='GET', **kwargs):
-        async with aiohttp.ClientSession() as session:
-            async with session.request(method, url, **kwargs) as response:
-                return Response(
-                    status=response.status,
-                    text=await response.text(),
-                    headers=response.headers,
-                    cookies=response.cookies,
-                    url=url
-                )
-    
-    async def worker(self, worker_id: int):
-        pass
-    
-    async def __launch_workers(self, num_workers: int):
-        await asyncio.gather(*[
-            asyncio.create_task(self.worker(i))
-            for i in range(num_workers)
-        ])   
-    
-    def scrape(self, num_workers: int):
-        try:
-            loop = asyncio.get_running_loop()
-        except RuntimeError: 
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-            
-        task = loop.create_task(self.__launch_workers(num_workers))
-        
-        if not loop.is_running():
-            loop.run_until_complete(task)
+	async def request(self, url: str, method: str = 'GET', **kwargs):
+		"""Make HTTP request."""
+		return await self.session.request(method, url, timeout=self.client_timeout, **kwargs)
+
+	@abstractmethod
+	async def worker(self, worker_id: int):
+		"""Override to define worker logic."""
+		pass
+
+	async def _scrape_async(self, num_workers: int):
+		"""Run workers concurrently."""
+		async with aiohttp.ClientSession(timeout=self.client_timeout) as session:
+			self.session = session
+			await asyncio.gather(*[
+				self.worker(i) for i in range(num_workers)
+			])
+
+	def run(self, num_workers: int):
+		"""Run workers concurrently. Sync entry point."""
+		asyncio.run(self._scrape_async(num_workers))
